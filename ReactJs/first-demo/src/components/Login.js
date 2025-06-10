f890474de5f62832a98985b48ee17a7623a2a2c6
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-10 10:05:16 +0530

    10Jun 10:05

--- ReactJs/first-demo/src/components/Login.js
+++ ReactJs/first-demo/src/components/Login.js
@@ -1,17 +1,29 @@
 import { Component } from "react";
 
  class Login extends Component{
+    
 
     constructor(props){
         super(props);
-        
+        this.state={username:'', password:'', result:''};
+        this.usernameChange=this.usernameChange.bind(this);
+        this.passwordChange=this.passwordChange.bind(this);
+    }
+
+    usernameChange(e){
+        this.setState({username:e.target.value})
+    }
+
+    passwordChange(e){
+        this.setState({password:e.target.value})
     }
 
     render(){
         return <div>
+            { JSON.stringify(this.state) }
             <h3>{this.props.title}</h3>
-            Username: <input type="text" className="form-control" />
-            Password: <input type="password" className="form-control" /><br/>
+            Username: <input type="text" className="form-control" onChange={this.usernameChange} />
+            Password: <input type="password" className="form-control" onChange={this.passwordChange} /><br/>
             <input type="button" value="Login" className="btn btn-success" />&nbsp;
             <input type="reset" value="Reset" className="btn btn-warning" /><br/>
         </div>
