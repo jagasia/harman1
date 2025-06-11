5de78c906458dfbb57628014da556079fcdccfac
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-11 09:57:10 +0530

    11Jun 09:57

--- ReactJs/first-demo/src/components/Register.js
+++ ReactJs/first-demo/src/components/Register.js
@@ -31,11 +31,23 @@ export const Register = () => {
         }
     }
 
+    function fnValidateEmail(email){
+        if(email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)){
+            setErrors(prev=>({...prev, email:''}));
+        }else{
+            setErrors(prev=>({...prev, email:'Invalid Email address'}));
+        }
+    }
+
     // 👇 Watch for password or cpassword changes
     useEffect(() => {
         fnValidatePassword(user.password);
         fnConfirmPassword(user.password, user.cpassword);
     }, [user.password, user.cpassword]);
+
+    useEffect(()=>{
+        fnValidateEmail(user.email);
+    },[user.email]);
 
     return (
         <div>
@@ -60,7 +72,8 @@ export const Register = () => {
                 }} />
                 {   touched.cpassword && <div className="text-danger">{errors.cpassword}</div>}
 
-                Email: <input type="email" className="form-control" onChange={(e) => setUser({ ...user, email: e.target.value })} />
+                Email: <input type="email" className={`form-control ${errors.email && touched.email && 'is-invalid'}`} onChange={(e) => {setUser({ ...user, email: e.target.value }); setTouched({...touched, email:true})}} />
+                {   touched.email && <div className="text-danger">{errors.email}</div>}
                 Date of Birth: <input type="date" className="form-control" onChange={(e) => setUser({ ...user, dateOfBirth: e.target.value })} />
                 Phone number: <input type="number" className="form-control" onChange={(e) => setUser({ ...user, phone: e.target.value })} />
                 <br />
