b021bb7bcd8def6bed57dc5060be045a2d063abf
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-12 10:20:03 +0530

    12Jun 10:19

---
+++ ReactJs/my-react-app/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import { About } from './components/About'
+import { Contact } from './components/Contact'
+import { Home } from './components/Home'
+import { Login } from './components/Login'
+import { Signup } from './components/Signup'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='p-5 bg-primary text-white text-center'>
+        <h1>React Routing Demo</h1>
+      </div>
+      <div className='row'>
+        <div className='col-md-4'></div>
+        <div className='col-md-4'>
+          <Routes>
+            <Route path='/home' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+
+          </Routes>
+        </div>
+        <div className='col-md-4'></div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ ReactJs/my-react-app/src/components/Contact.jsx
@@ -0,0 +1,5 @@
+export const Contact=()=>{
+    return <>
+        Contact us page
+    </>
+}
