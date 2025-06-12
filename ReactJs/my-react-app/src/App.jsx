3eb85c13ed0111dfbd29c8772410bf0576cc6703
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-12 10:45:10 +0530

    12Jun 10:45

--- ReactJs/my-react-app/src/App.jsx
+++ ReactJs/my-react-app/src/App.jsx
@@ -5,6 +5,8 @@ import { About } from './components/About'
 import { Contact } from './components/Contact'
 import { Home } from './components/Home'
 import { Login } from './components/Login'
+import { Nav } from './components/Nav'
+import { NotFound } from './components/NotFound'
 import { Signup } from './components/Signup'
 
 function App() {
@@ -12,19 +14,21 @@ function App() {
 
   return (
     <>
+      <Nav />
       <div className='p-5 bg-primary text-white text-center'>
         <h1>React Routing Demo</h1>
       </div>
+      
       <div className='row'>
         <div className='col-md-4'></div>
         <div className='col-md-4'>
           <Routes>
-            <Route path='/home' element={<Home />} />
+            <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
             <Route path='/contact' element={<Contact />} />
             <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<Signup />} />
-
+            <Route path='/*' element={<NotFound />} />
           </Routes>
         </div>
         <div className='col-md-4'></div>
