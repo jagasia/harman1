44de11d1085ee70f0a193e10ad273219e8d6c474
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-13 15:43:26 +0530

    13Jun 15:43

--- ReactJs/advanced/src/App.jsx
+++ ReactJs/advanced/src/App.jsx
@@ -1,8 +1,27 @@
-import './App.css'
-import { Counter } from './components/Counter'
-import { ProductManager } from './components/ProductManager'
+import { Profiler } from 'react';
+import './App.css';
+import { Counter } from './components/Counter';
+import { ProductManager } from './components/ProductManager';
 
 function App() {
+  
+   function onRenderCallback(
+    id, // the "id" prop of the Profiler tree
+    phase, // either "mount" or "update"
+    actualDuration, // time spent rendering the committed update
+    baseDuration, // estimated time to render without memoization
+    startTime, // when React began rendering this update
+    commitTime, // when React committed this update
+    interactions // the Set of interactions belonging to this update
+  ) {
+    console.log(`[Profiler: ${id}]`);
+    console.log(`Phase: ${phase}`);
+    console.log(`Actual duration: ${actualDuration.toFixed(2)}ms`);
+    console.log(`Base duration: ${baseDuration.toFixed(2)}ms`);
+    console.log(`Start time: ${startTime}`);
+    console.log(`Commit time: ${commitTime}`);
+    console.log("Interactions:", interactions);
+  }
   
   return (
     <>
@@ -15,7 +34,9 @@ function App() {
         </div>
         <div className='col-md-8'>
           {/* <ProductForm /> */}
-          <ProductManager />
+          <Profiler id='ProductManager' onRender={onRenderCallback}>
+            <ProductManager />
+          </Profiler>
         </div>
         <div className='col-md-2'>
           
