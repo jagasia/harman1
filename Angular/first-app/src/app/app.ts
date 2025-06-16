392f2926f2ab2f0c1bae48eb8569510ca8b5c60c
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-16 16:32:24 +0530

    16Jun 16:32

--- Angular/first-app/src/app/app.ts
+++ Angular/first-app/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { Country } from "./country/country";
+import { SwitchDemo } from "./switch-demo/switch-demo";
 
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, Country],
+  imports: [FormsModule, SwitchDemo],
   templateUrl: './app.html',
   // template:`Hi this is template`,
   styleUrl: './app.css'
