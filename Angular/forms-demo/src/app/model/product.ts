e8f17369ec1338d2c6acfe540967dbacb870ef31
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-17 12:04:24 +0530

    17Jun 12:04

--- Angular/forms-demo/src/app/model/product.ts
+++ Angular/forms-demo/src/app/model/product.ts
@@ -3,10 +3,14 @@ export class Product {
     name:string='';
     category:string='';
     price:number=0;
+   
     constructor(id:number, name:string, category:string, price:number){
         this.id=id;
         this.name=name;
         this.category=category;
         this.price=price;
     }
+
+   
+
 }
--- Angular/forms-demo/src/app/product-form/product-form.ts
+++ Angular/forms-demo/src/app/product-form/product-form.ts
@@ -12,4 +12,16 @@ import { Product } from '../model/product';
 export class ProductForm {
   // product:Product={id:0, name:'',category:'',price:0};
   product:Product=new Product(0,'','',0);
+  errors:any={};
+
+  fnValidateId(){
+    console.log('validating id');
+    
+    //must be 3 digit positive number
+    if(this.product.id<100){
+      this.errors.id='Product Id must be 3 digit number';
+    }else{
+      this.errors.id='';
+    }
+  }
 }
