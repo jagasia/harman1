956449f0e0af6749aa55005d00a0187eee387da0
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-17 11:55:45 +0530

    17Jun 11:55

--- Angular/forms-demo/src/app/app.ts
+++ Angular/forms-demo/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { EmployeeForm } from "./employee-form/employee-form";
 import { Highlight } from './highlight';
 import { ProductForm } from './product-form/product-form';
 
 @Component({
   selector: 'app-root',
-  imports: [Highlight, ProductForm],
+  imports: [Highlight, ProductForm, EmployeeForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
