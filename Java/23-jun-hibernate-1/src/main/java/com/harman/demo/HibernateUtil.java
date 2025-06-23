70652a8bebed17d620afe33a7e17adde57840237
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-23 17:04:30 +0530

    23Jun 17:04

---
+++ Java/23-jun-hibernate-1/src/main/java/com/harman/demo/HibernateUtil.java
@@ -0,0 +1,24 @@
+package com.harman.demo;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import com.harman.demo.model.Product;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(Product.class)
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
