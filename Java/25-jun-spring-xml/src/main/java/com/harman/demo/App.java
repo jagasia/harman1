ca00201a4375510223e1b6ca2357255087c77d46
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-25 10:19:03 +0530

    25Jun 10:18

--- Java/25-jun-spring-xml/src/main/java/com/harman/demo/App.java
+++ Java/25-jun-spring-xml/src/main/java/com/harman/demo/App.java
@@ -3,6 +3,7 @@ package com.harman.demo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.harman.demo.model.Book;
 import com.harman.demo.model.Calculator;
 
 /**
@@ -14,8 +15,16 @@ public class App
     public static void main( String[] args )
     {
     	ApplicationContext ctx=new ClassPathXmlApplicationContext("spring.xml");
-    	Calculator calc = ctx.getBean(Calculator.class);
-    	System.out.println(calc.sum());
-        System.out.println( "Hello World!" );
+//    	Calculator calc = ctx.getBean(Calculator.class);
+//    	System.out.println(calc.sum());
+//        System.out.println( "Hello World!" );
+    	
+    	Book book1=(Book) ctx.getBean("b1");
+    	book1.setTitle("Tale of two cities");
+    	
+    	Book book2=(Book) ctx.getBean("b1"); 
+    	
+    	System.out.println(book1);
+    	System.out.println(book2);
     }
 }
