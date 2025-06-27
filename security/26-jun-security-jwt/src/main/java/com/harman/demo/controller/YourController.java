0367cc4bf284ad1d338e6114c8338299fb749415
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-27 12:04:41 +0530

    27Jun 12:04

--- security/26-jun-security-jwt/src/main/java/com/harman/demo/controller/YourController.java
+++ security/26-jun-security-jwt/src/main/java/com/harman/demo/controller/YourController.java
@@ -1,17 +1,21 @@
 package com.harman.demo.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/api/v2")
 public class YourController {
 
 	@GetMapping("/about")
+	@SecurityRequirement(name = "Bearer Authentication")
 	@PreAuthorize("hasAuthority('user')")
-	public String about() {
+	public String about(Authentication authentication) {
 		return "About us";
 	}
 	
