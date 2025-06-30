fef8ddcfa23b19bec3107560de0055bc1ddf4505
jagasia <rjagadeeswaran@Yahoo.com>
2025-06-30 10:40:52 +0530

    30Jun 10:40

---
+++ microservices/sonar-1/src/main/java/Example.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Example {
+
+	public String speak() {
+		int i;
+		Date dt=null;
+		System.out.println(dt.toString());
+		int k=0;
+		if(k==0)
+			return "Ok";
+			
+		return "Hello";
+		
+	}
+}
